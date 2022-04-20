78acdeafc87b698fd874beac82abb74f2c6686a5
JUYOUNG KIM <wndud2274@gmail.com>
2022-04-20 21:58:52 +0900

    [update] CRUD 기초 : R

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
-@Controller('boards')
+@Controller('boards') //http://localhost:3000/boards
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard() {
+    return this.boardsService.getAllBoards();
+  }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  //일단 DB는 제외하고 구현
+  private boards = []; //private를 사용해야 다른 컴포넌트에서 수정 불가
+
+  getAllBoards() {
+    return this.boards;
+  }
+}
