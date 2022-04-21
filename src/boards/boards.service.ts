41ed254b5e0eeac6ce6b57a05b13db1a15fdbe2a
JUYOUNG KIM <wndud2274@gmail.com>
2022-04-21 22:44:12 +0900

    [update] getAllBoards() -> type 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
   //일단 DB는 제외하고 구현
-  private boards = []; //private를 사용해야 다른 컴포넌트에서 수정 불가
+  private boards: Board[] = []; //private를 사용해야 다른 컴포넌트에서 수정 불가
 
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boards;
   }
 }
