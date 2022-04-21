b9419c2bdb6e11add5b5166cf906fc4d8b08fc67
JUYOUNG KIM <wndud2274@gmail.com>
2022-04-21 22:42:59 +0900

    [update] @Get -> type 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards') //http://localhost:3000/boards
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Get('/')
-  getAllBoard() {
+  getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
   }
 }
