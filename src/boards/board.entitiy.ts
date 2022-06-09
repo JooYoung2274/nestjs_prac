9698e767214a9c449d5637861c94081b848e3054
JooYoung Kim <66576346+JooYoung2274@users.noreply.github.com>
2022-06-09 17:52:24 +0900

    [add] Board.Entity

---
+++ src/boards/board.entitiy.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './board.model';
+
+@Entity()
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: BoardStatus;
+}
