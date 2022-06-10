6c3c52f54cbbef43b1c7d721ba6a0b98d34089c4
JooYoung Kim <66576346+JooYoung2274@users.noreply.github.com>
2022-06-10 13:43:45 +0900

    Update boards.controller.ts

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -4,50 +4,44 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipe/board-status-validation.pipe';
+import { Board } from './board.entity';
+import { identity } from 'rxjs';
 
 @Controller('boards') //http://localhost:3000/boards
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get('/')
-  getAllBoard(): Board[] {
-    return this.boardsService.getAllBoards();
-  }
-
-  // 전체를 받아오고 싶으면 @Body() body 같이 작성함
-  // 하나씩 받아오고 싶으면 아래처럼
-  // @Body('title') title: string,
-  // @Body('description') description: string,
-  @Post('/')
-  @UsePipes(ValidationPipe) //pipe (내장 pipe 사용)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    return this.boardsService.createBoards(createBoardDto);
-  }
-
   @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
+  getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardsService.getBoardById(id);
   }
 
+  @Post('/')
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
   @Delete('/:id')
-  deleteBoard(@Param('id') id: string): void {
-    this.boardsService.deleteBoard(id);
+  deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.boardsService.deleteBoard(id);
   }
 
   @Patch('/:id/status')
   updateBoardStatus(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ): Board {
+  ): Promise<Board> {
     return this.boardsService.updateBoardStatus(id, status);
   }
 }
