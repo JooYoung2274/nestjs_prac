17cb561ad3fea83bb0d4330d83f6a4999a870ba8
JooYoung Kim <66576346+JooYoung2274@users.noreply.github.com>
2022-06-10 13:42:31 +0900

    Update board.model.ts

--- src/boards/board.model.ts
+++ src/boards/board.model.ts
@@ -1,11 +1,12 @@
-export interface Board {
-  id: string;
-  title: string;
-  description: string;
-  status: BoardStatus;
-}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
+import { BoardsController } from './boards.controller';
+import { BoardsService } from './boards.service';
 
-export enum BoardStatus {
-  PUBLIC = 'PUBLIC',
-  PRIVATE = 'PRIVATE',
-}
+@Module({
+  imports: [TypeOrmModule.forFeature([BoardRepository])],
+  controllers: [BoardsController],
+  providers: [BoardsService],
+})
+export class BoardsModule {}
