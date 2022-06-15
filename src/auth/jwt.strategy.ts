ede80829eafb71d65cccbc5451577dabc6f18bc5
JooYoung Kim <jooooo2274@gmail.com>
2022-06-15 18:17:37 +0900

    [add] jwt.strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from './user.entity';
+
+import { UserRepository } from './user.repository';
+
+@Injectable() //다른 곳에서도 사용할 수 있게
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
+    super({
+      secretOrKey: '시크릿 키',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload) {
+    const { username } = payload;
+    const user: User = await this.userRepository.findUserByUsername(username);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
