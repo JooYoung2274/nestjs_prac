0eb3951ed1580f5299595d2a608bf0cd6abd3834
JooYoung Kim <jooooo2274@gmail.com>
2022-06-15 18:18:14 +0900

    [update] jwt, passport 로직 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }), //passport, jwt import
+    JwtModule.register({
+      secret: '시크릿키입력. 환경변수로 숨김',
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
