bde0f1831a926a98fcfd4b20290d5457269b4e06
JooYoung Kim <jooooo2274@gmail.com>
2022-06-15 16:35:03 +0900

    [update] signin repository

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,19 +1,18 @@
-import {
-  ConflictException,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto.ts';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const user = this.create({
-      username,
-      password,
-    });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
@@ -24,5 +23,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  async findUserByUsername(username: string) {
+    return await this.findOne(username);
   }
 }
