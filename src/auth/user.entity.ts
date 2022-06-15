c6ddf05f3a1fac9814b4cab7202525ef06da5bdb
JooYoung Kim <jooooo2274@gmail.com>
2022-06-15 15:29:12 +0900

    [update] 'username' 중복 에러 처리

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['username']) //'username'은 유니크 해야함. 같은게 있으면 에러
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
