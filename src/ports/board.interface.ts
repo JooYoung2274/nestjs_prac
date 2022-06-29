9513d82c269ea8508a6efea61f725e346cd20491
JooYoung Kim <jooooo2274@gmail.com>
2022-06-29 15:29:21 +0900

    [fix] IBoardService 수정, IBoardRepository 추가

--- src/ports/board.interface.ts
+++ src/ports/board.interface.ts
@@ -2,11 +2,20 @@ import { User } from 'src/auth/user.entity';
 import { BoardStatus } from 'src/boards/board-status.enum';
 import { Board } from 'src/boards/board.entity';
 import { CreateBoardDto } from 'src/boards/dto/create-board.dto';
+import { DeleteResult } from 'typeorm';
 
-export interface BoardService {
-  getBoardById(id: number): Promise<Board>;
-  getAllBoards(user: User): Promise<Board[]>;
-  createBoard(createBoardDto: CreateBoardDto, user: User): Promise<Board>;
-  deleteBoard(id: number, user: User): Promise<void>;
-  updateBoardStatus(id: number, status: BoardStatus): Promise<Board>;
+export interface IBoardService {
+  getBoardById: (id: number) => Promise<Board>;
+  getAllBoards: (user: User) => Promise<Board[]>;
+  createBoard: (createBoardDto: CreateBoardDto, user: User) => Promise<Board>;
+  deleteBoard: (id: number, user: User) => Promise<void>;
+  updateBoardStatus: (id: number, status: BoardStatus) => Promise<Board>;
+}
+
+export interface IBoardRepository {
+  findBoardById: (id: number) => Promise<Board>;
+  findAllBoardsByUserId: (user: User) => Promise<Board[]>;
+  createBoard: (createBoardDto: CreateBoardDto, user: User) => Promise<Board>;
+  deleteBoardById: (id: number, user: User) => Promise<DeleteResult>;
+  updateBoardStatus: (id: number, status: BoardStatus) => Promise<Board>;
 }
